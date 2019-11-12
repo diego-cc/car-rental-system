cb2af3529710633c5fd2f29196776f1394b28680
Diego Craveiro Chaves <20026893@tafe.wa.edu.au>
2019-11-12 20:11:52 +0800

    using react-bootstrap-typeahead (much easier!) instead of react-autosuggest

--- src/components/Navigation/SearchVehicle.js
+++ src/components/Navigation/SearchVehicle.js
@@ -1,80 +1,27 @@
 import React from 'react';
-import Autosuggest from 'react-autosuggest';
-import {AppContext} from "../../AppContext/AppContext";
-import {Form, ListGroup} from "react-bootstrap";
-import FormContext from "react-bootstrap/cjs/FormContext";
+import { Typeahead } from 'react-bootstrap-typeahead';
+import {AppConsumer, AppContext} from "../../AppContext/AppContext";
 
 export class SearchVehicle extends React.Component {
-  constructor(props) {
-	super(props);
-	this.state = {
-	  value: '',
-	  suggestions: []
-	};
-  }
-
-  static contextType = AppContext;
-
-  getSuggestions = value => {
-	const inputValue = value.trim().toLowerCase();
-	const inputLength = inputValue.length;
-
-	return inputLength === 0 ? [] : this.context.vehicles.filter(v => (
-	  v.manufacturer.trim().toLowerCase().slice(0, inputLength) === inputValue ||
-	  v.model.trim().toLowerCase().slice(0, inputLength) === inputValue ||
-	  v.year.trim().slice(0, inputLength) === inputValue
-	));
-  };
-
-  getSuggestionValue = suggestion => suggestion.id;
-
-  handleSubmit = e => {
-
-  };
-
-  renderSuggestion = suggestion => (
-	<ListGroup>
-	  <ListGroup.Item as="li" active>
-		{`${suggestion.manufacturer} ${suggestion.model} (${suggestion.year})`}
-	  </ListGroup.Item>
-	</ListGroup>
-  );
-
-  onChange = (e, {newValue}) => {
-	this.setState({
-	  value: newValue
-	})
-  };
-
-  onSuggestionsFetchRequested = ({value}) => {
-	this.setState({
-	  suggestions: this.getSuggestions(value)
-	})
-  };
-
-  onSuggestionsClearRequested = () => {
-	this.setState({
-	  suggestions: []
-	})
-  };
 
   render() {
-	const {value, suggestions} = this.state;
-	const inputProps = {
-	  placeholder: 'Search for a vehicle...',
-	  value,
-	  onChange: this.onChange
-	};
-
 	return (
-	  <Autosuggest
-		suggestions={suggestions}
-		onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-		onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-		getSuggestionValue={this.getSuggestionValue}
-		renderSuggestion={this.renderSuggestion}
-		inputProps={inputProps}
-	  />
-	);
+	  <AppConsumer>
+		{
+		  ({vehicles}) => (
+			<Typeahead
+			  labelKey={(option) => `${option.manufacturer} ${option.model} (${option.year})`}
+			  filterBy={['manufacturer', 'model', 'year']}
+			  options={vehicles}
+			  placeholder="Search for a vehicle..."
+			  highlightOnlyResult
+			  minLength="1"
+			  />
+		  )
+		}
+	  </AppConsumer>
+	)
   }
 }
+
+SearchVehicle.contextType = AppContext;
--- src/components/ShowVehicle/ShowVehicle.js
+++ src/components/ShowVehicle/ShowVehicle.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import {AppConsumer} from "../../AppContext/AppContext";
 
 export const ShowVehicle = props => {
+  	return (
+  	  <AppConsumer>
 
-}
+	  </AppConsumer>
+	)
+};
