24fadcd7b4fcaea28bcd24246c68729052b65482
Diego Craveiro Chaves <20026893@tafe.wa.edu.au>
2019-11-20 20:11:51 +0800

    Updates

---
+++ precache-manifest.a57a719da6be4590f7c2a79ba038745e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc3b74bf503a80dbdb185907f0148e8d",
+    "url": "/car-rental-system/index.html"
+  },
+  {
+    "revision": "3262e54e2017c824c53b",
+    "url": "/car-rental-system/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "e2a95a326899e2ecb984",
+    "url": "/car-rental-system/static/css/main.a0b1a750.chunk.css"
+  },
+  {
+    "revision": "3262e54e2017c824c53b",
+    "url": "/car-rental-system/static/js/2.9259e66d.chunk.js"
+  },
+  {
+    "revision": "e2a95a326899e2ecb984",
+    "url": "/car-rental-system/static/js/main.5edf8f59.chunk.js"
+  },
+  {
+    "revision": "b3f8b4763f8601f0d9bc",
+    "url": "/car-rental-system/static/js/runtime-main.7fee94cc.js"
+  }
+]);
