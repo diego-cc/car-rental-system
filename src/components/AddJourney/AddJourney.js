46246bb108a2535f12ea17f4c7b645e79ca75b6a
Diego Craveiro Chaves <20026893@tafe.wa.edu.au>
2019-11-24 03:31:16 +0800

    refactored AddJourney and AddFuelPurchase

--- src/components/AddJourney/AddJourney.js
+++ src/components/AddJourney/AddJourney.js
@@ -1,163 +1,134 @@
 /**
  * AddJourney.js
  */
-import React from 'react';
+import React, {useContext} from 'react';
 import {AppConsumer, AppContext} from "../../AppContext/AppContext";
 import {Accordion, Button, Card, Col, Container, ListGroup, Row} from "react-bootstrap";
 import {Notification} from "../Notification/Notification";
 import {LoadingSpinner} from "../LoadingSpinner/LoadingSpinner";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faPlus} from "@fortawesome/free-solid-svg-icons/faPlus";
-import {Link} from "react-router-dom";
+import {Link, useParams, useHistory} from "react-router-dom";
+const cloneDeep = require('lodash.clonedeep');
 
 /**
  * AddJourney component - navigates to AddJourneyForm once a booking is selected
  */
-export class AddJourney extends React.Component {
-  constructor(props) {
-	super(props);
-	this.initialState = {
-	  vehicle: '',
-	  fields: {
-		id: '',
-		bookingID: '',
-		journeyStartedAt: '',
-		journeyEndedAt: '',
-		journeyStartOdometerReading: '',
-		journeyEndOdometerReading: '',
-		journeyFrom: '',
-		journeyTo: '',
-		createdAt: '',
-		updatedAt: null
+export const AddJourney = () => {
+  const {vehicles} = useContext(AppContext);
+  const {vehicleID} = useParams();
+  const vehicle = cloneDeep(vehicles.find(v => v.id === vehicleID));
+  const history = useHistory();
+
+  return (
+	<AppConsumer>
+	  {
+		({loading, notification}) => (
+		  <Container>
+			{
+			  notification.display ?
+				(
+				  <Notification
+					display={notification.display}
+					message={notification.message}/>
+				) : ''
+			}
+			<Row>
+			  <Col>
+				<h2 className="text-center my-5">
+				  Select a booking to add a new journey for {vehicle ?
+				  `${vehicle.manufacturer} ${vehicle.model} (${vehicle.year})`
+				  : ''}
+				</h2>
+			  </Col>
+			</Row>
+			{
+			  loading ?
+				(
+				  <Row className="justify-content-center mt-5">
+					<LoadingSpinner/>
+				  </Row>
+				) :
+				(
+				  <Accordion>
+					{
+					  vehicle ?
+						(
+						  vehicle
+							.bookings
+							.filter(booking => booking.vehicleID === vehicle.id)
+							.sort((booking1, booking2) => {
+							  const booking1StartDate = new Date(booking1.startDate);
+							  const booking2StartDate = new Date(booking2.startDate);
+
+							  if (booking1StartDate > booking2StartDate) {
+								return -1;
+							  } else if (booking1StartDate < booking2StartDate) {
+								return 1;
+							  }
+							  return 0;
+							})
+							.map((booking, index) => (
+							  <Card key={index} style={{overflow: 'visible'}}>
+								<Card.Header>
+								  <Accordion.Toggle
+									className="mr-auto"
+									as={Button}
+									variant="link"
+									eventKey={index}>
+									{`${new Date(booking.startDate).toLocaleDateString('en-AU')}`}
+								  </Accordion.Toggle>
+								  <Link to={`/addJourneyForm/${booking.id}`}>
+									<Button variant="outline-success">
+									  <FontAwesomeIcon icon={faPlus}/>
+									</Button>
+								  </Link>
+								</Card.Header>
+								<Accordion.Collapse eventKey={index}>
+								  <Card.Body>
+									<ListGroup>
+									  <ListGroup.Item>
+										Start
+										Date: {`${new Date(booking.startDate).toLocaleDateString('en-AU')}`}
+									  </ListGroup.Item>
+									  <ListGroup.Item>
+										End
+										Date: {`${new Date(booking.endDate).toLocaleDateString('en-AU')}`}
+									  </ListGroup.Item>
+									  <ListGroup.Item>
+										Start Odometer: {booking.startOdometer} km
+									  </ListGroup.Item>
+									  <ListGroup.Item>
+										Booking
+										Type: {booking.bookingType === 'D' ? 'Per day' : 'Per km'}
+									  </ListGroup.Item>
+									</ListGroup>
+								  </Card.Body>
+								</Accordion.Collapse>
+							  </Card>
+							))
+						)
+						: ''
+					}
+				  </Accordion>
+				)
+			}
+			<Button
+			  className="my-5"
+			  style={{
+				position: 'relative',
+				left: '50%',
+				transform: 'translateX(-50%)'
+			  }}
+			  variant="danger"
+			  size="lg"
+			  onClick={() => history.push(`/show/${vehicle.id}`)}
+			>
+			  Cancel
+			</Button>
+		  </Container>
+		)
 	  }
-	};
-	this.state = {...this.initialState};
-  }
-
-  componentDidMount() {
-	const {vehicleID} = this.props.match.params;
-	const {vehicles} = this.context;
-
-	const vehicle = vehicles.find(vehicle => vehicle.id === vehicleID);
-	this.setState({
-	  ...this.initialState,
-	  vehicle
-	});
-  }
-
-  render() {
-	return (
-	  <AppConsumer>
-		{
-		  ({loading, notification}) => (
-			<Container>
-			  {
-				notification.display ?
-				  (
-					<Notification
-					  display={notification.display}
-					  message={notification.message}/>
-				  ) : ''
-			  }
-			  <Row>
-				<Col>
-				  <h2 className="text-center my-5">
-					Select a booking to add a new journey for {this.state.vehicle ?
-					`${this.state.vehicle.manufacturer} ${this.state.vehicle.model} (${this.state.vehicle.year})`
-					: ''}
-				  </h2>
-				</Col>
-			  </Row>
-			  {
-				loading ?
-				  (
-					<Row className="justify-content-center mt-5">
-					  <LoadingSpinner/>
-					</Row>
-				  ) :
-				  (
-					<Accordion>
-					  {
-						this.state.vehicle ?
-						  (
-							this.state.vehicle
-							  .bookings
-							  .filter(booking => booking.vehicleID === this.state.vehicle.id)
-							  .sort((booking1, booking2) => {
-								const booking1StartDate = new Date(booking1.startDate);
-								const booking2StartDate = new Date(booking2.startDate);
-
-								if (booking1StartDate > booking2StartDate) {
-								  return -1;
-								} else if (booking1StartDate < booking2StartDate) {
-								  return 1;
-								}
-								return 0;
-							  })
-							  .map((booking, index) => (
-								<Card key={index} style={{overflow: 'visible'}}>
-								  <Card.Header>
-									<Accordion.Toggle
-									  className="mr-auto"
-									  as={Button}
-									  variant="link"
-									  eventKey={index}>
-									  {`${new Date(booking.startDate).toLocaleDateString('en-AU')}`}
-									</Accordion.Toggle>
-									<Link to={`/addJourneyForm/${booking.id}`}>
-									  <Button variant="outline-success">
-										<FontAwesomeIcon icon={faPlus}/>
-									  </Button>
-									</Link>
-								  </Card.Header>
-								  <Accordion.Collapse eventKey={index}>
-									<Card.Body>
-									  <ListGroup>
-										<ListGroup.Item>
-										  Start
-										  Date: {`${new Date(booking.startDate).toLocaleDateString('en-AU')}`}
-										</ListGroup.Item>
-										<ListGroup.Item>
-										  End
-										  Date: {`${new Date(booking.endDate).toLocaleDateString('en-AU')}`}
-										</ListGroup.Item>
-										<ListGroup.Item>
-										  Start Odometer: {booking.startOdometer} km
-										</ListGroup.Item>
-										<ListGroup.Item>
-										  Booking
-										  Type: {booking.bookingType === 'D' ? 'Per day' : 'Per km'}
-										</ListGroup.Item>
-									  </ListGroup>
-									</Card.Body>
-								  </Accordion.Collapse>
-								</Card>
-							  ))
-						  )
-						  : ''
-					  }
-					</Accordion>
-				  )
-			  }
-			  <Button
-				className="my-5"
-				style={{
-				  position: 'relative',
-				  left: '50%',
-				  transform: 'translateX(-50%)'
-				}}
-				variant="danger"
-				size="lg"
-				onClick={() => this.props.history.push(`/show/${this.state.vehicle.id}`)}
-			  >
-				Cancel
-			  </Button>
-			</Container>
-		  )
-		}
-	  </AppConsumer>
-	)
-  }
-}
-
-AddJourney.contextType = AppContext;
+	</AppConsumer>
+  )
+};
