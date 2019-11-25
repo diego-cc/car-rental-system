92db4601692405035579ae09258f54c928e2f21f
Diego Craveiro Chaves <20026893@tafe.wa.edu.au>
2019-11-26 00:50:09 +0800

    added tests for AddService

---
+++ src/components/AddService/AddService.test.js
@@ -0,0 +1,88 @@
+/**
+ * AddService.test.js
+ */
+import React from 'react';
+import {act, fireEvent, render, wait} from '@testing-library/react';
+import {MemoryRouter, Route} from 'react-router-dom';
+import {fakeAPI} from "../../setupTests";
+import {AddService} from "./AddService";
+import '@testing-library/jest-dom/extend-expect'
+import {AppProvider} from "../../AppContext/AppContext";
+import moment from "moment";
+
+const initialContextValue = {
+  vehicles: fakeAPI.vehicles.map(v => {
+	const vehicleServices = fakeAPI.services.reduce((vServices, s) => {
+	  if (s.vehicleID === v.id) {
+		vServices.push(s);
+	  }
+	  return vServices;
+	}, []);
+	v.services.push(...vehicleServices);
+	return v;
+  }),
+  addResource: (resourceType, resource) => {
+	if (resourceType.trim().toLowerCase() === 'service') {
+	  contextValue.vehicles.find(v => v.id === resource.vehicleID).services.push(resource);
+	}
+  }
+};
+
+let tree, contextValue;
+
+beforeEach(() => {
+  contextValue = {...initialContextValue};
+  tree = (
+	<AppProvider value={contextValue}>
+	  <MemoryRouter initialEntries={[`/addService/tesla-123`]}>
+		<Route path={`/addService/:vehicleID`} render={props => <AddService {...props} />}/>
+	  </MemoryRouter>
+	</AppProvider>
+  );
+});
+
+describe('AddService component', () => {
+  it('loads AddService form with correct default values', async () => {
+	let component;
+	component = render(tree);
+	const {getByLabelText} = component;
+
+	await wait(() => {
+	  expect(getByLabelText(/^Serviced at:/)).toHaveValue(moment(moment(), 'YYYY-MM-DD').format('YYYY-MM-DD'));
+	  expect(getByLabelText(/^Service odometer:/)).toHaveValue(500);
+	});
+  });
+
+  it('does not add a new service right away if there are service conflicts', async () => {
+	let component;
+	component = render(tree);
+	const {getByText, getByLabelText} = component;
+	const servicedAtInput = getByLabelText(/^Serviced at:/);
+
+	act(() => {
+	  fireEvent.change(servicedAtInput, {target: {value: moment(moment().add(10, 'days'), 'YYYY-MM-DD').format('YYYY-MM-DD')}});
+	  fireEvent.click(getByText('Add service'));
+	});
+
+	await wait(() => {
+	  const vehicle = contextValue.vehicles.find(v => v.id === 'tesla-123');
+	  expect(vehicle.services.length).toBe(1);
+	});
+  });
+
+  it('adds a new service if there are no conflicts', async () => {
+	let component;
+
+	component = render(tree);
+	const {getByText} = component;
+
+	act(() => {
+	  fireEvent.click(getByText('Add service'));
+	});
+
+	await wait(() => {
+	  const vehicle = contextValue.vehicles.find(v => v.id === 'tesla-123');
+	  expect(vehicle.services.length).toBe(2);
+	});
+  })
+});
