6ceccd4ad669643d247cb8039a97974896023df8
diego-cc <diego.cc.github@gmail.com>
2019-12-05 11:40:15 +0800

    filter vehicle on enter and show all vehicles on clear

--- src/components/Navigation/SearchVehicle.js
+++ src/components/Navigation/SearchVehicle.js
@@ -1,11 +1,11 @@
 /**
  * SearchVehicle.js
  */
-import React, {useRef, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {Typeahead} from 'react-bootstrap-typeahead';
 import {AppConsumer} from "../../AppContext/AppContext";
 import {Button, Form, FormGroup} from "react-bootstrap";
-import { useHistory } from 'react-router-dom';
+import {useHistory} from 'react-router-dom';
 
 /**
  * SearchVehicle component - allows the user to search for a vehicle (with typeahead) by
@@ -19,46 +19,55 @@ export const SearchVehicle = () => {
   const history = useHistory();
 
   const handleSubmit = e => {
-	e.preventDefault();
+    e.preventDefault();
 
-	if (selected && selected.length) {
-	  history.push(`/show/${selected[0].id}`);
-	}
+    if (selected && selected.length) {
+      history.push(`/show/${selected[0].id}`);
+    }
   };
 
+  useEffect(() => {
+    if (selected.length) {
+      history.push(`/show/${selected[0].id}`);
+    }
+  }, [selected, typeaheadRef]);
+
   return (
-	<AppConsumer>
-	  {
-		({vehicles}) => (
-		  <Form inline onSubmit={handleSubmit} className="mr-md-3">
-			<FormGroup>
-			  <Typeahead
-				id="search-vehicle"
-				labelKey={(option) => `${option.manufacturer} ${option.model} (${option.year})`}
-				filterBy={['manufacturer', 'model', 'year']}
-				options={vehicles}
-				placeholder="Search for a vehicle..."
-				highlightOnlyResult
-				minLength={1}
-				onChange={selected => setSelected(selected)}
-				ref={typeaheadRef}
-			  />
-			  <Button variant="outline-primary" type="submit" className="ml-sm-3">
-				Search
-			  </Button>
-			  <Button
-				onClick={() => {
-				  typeaheadRef.current.clear();
-				  typeaheadRef.current.focus();
-				}}
-				variant="outline-danger"
-				className="ml-sm-3">
-				Clear
-			  </Button>
-			</FormGroup>
-		  </Form>
-		)
-	  }
-	</AppConsumer>
+    <AppConsumer>
+      {
+        ({vehicles}) => (
+          <Form inline onSubmit={handleSubmit} className="mr-md-3">
+            <FormGroup>
+              <Typeahead
+                id="search-vehicle"
+                labelKey={(option) => `${option.manufacturer} ${option.model} (${option.year})`}
+                filterBy={['manufacturer', 'model', 'year']}
+                options={vehicles}
+                placeholder="Search for a vehicle..."
+                highlightOnlyResult
+                minLength={1}
+                onChange={selected => setSelected(selected)}
+                ref={typeaheadRef}
+                selectHintOnEnter={true}
+                submitFormOnEnter={selected[0]}
+              />
+              <Button variant="outline-primary" type="submit" className="ml-sm-3">
+                Search
+              </Button>
+              <Button
+                onClick={() => {
+                  typeaheadRef.current.clear();
+                  typeaheadRef.current.focus();
+                  history.push(`/browse`);
+                }}
+                variant="outline-danger"
+                className="ml-sm-3">
+                Clear
+              </Button>
+            </FormGroup>
+          </Form>
+        )
+      }
+    </AppConsumer>
   )
 };
